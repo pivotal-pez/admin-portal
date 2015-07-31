41662be96953345cc6537cc89799b1836b558e80
John Calabrese <jcalabrese@pivotal.io>
2015-07-31 15:16:38 -0400

    [#99570832] adding buildpack breakdown
    we now have a donut display of buildpack usage.

--- public/js/apps.js
+++ public/js/apps.js
@@ -1,4 +1,4 @@
-angular.module('AppsApp', [])
+angular.module('AppsApp', ['tc.chartjs'])
   .controller('AppsController', function($scope, $http, $interval, $timeout, $window) {
     var apps = this;
     var refreshInterval = 30000;
@@ -8,11 +8,47 @@ angular.module('AppsApp', [])
       empty: "-"
     };
 
-    $interval( function(){ users.getAppsInfo(); }, refreshInterval, 0, true);
+    $interval( function(){ apps.getAppsInfo(); }, refreshInterval, 0, true);
 
     $timeout(function () {
       apps.getAppsInfo();
     }, 1);
+
+    // Chart.js Options
+    $scope.options =  {
+      // Sets the chart to be responsive
+      responsive: true,
+      //Boolean - Whether we should show a stroke on each segment
+      segmentShowStroke : true,
+      //String - The colour of each segment stroke
+      segmentStrokeColor : '#fff',
+      //Number - The width of each segment stroke
+      segmentStrokeWidth : 2,
+      //Number - The percentage of the chart that we cut out of the middle
+      percentageInnerCutout : 50, // This is 0 for Pie charts
+      //Number - Amount of animation steps
+      animationSteps : 100,
+      //String - Animation easing effect
+      animationEasing : 'easeOutBounce',
+      //Boolean - Whether we animate the rotation of the Doughnut
+      animateRotate : true,
+      //Boolean - Whether we animate scaling the Doughnut from the centre
+      animateScale : false,
+      //String - A legend template
+      legendTemplate : '<ul class="tc-chart-js-legend"><% for (var i=0; i<segments.length; i++){%><li><span style="background-color:<%=segments[i].fillColor%>"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>'
+    };
+
+    apps.setDonutUX = function(data) {
+      $scope.data = [
+        { value: data.JavaBPCount, color: '#F7464A', highlight: '#FF5A5E', label: 'Java'},
+        { value: data.RubyBPCount, color: '#46BFBD', highlight: '#5AD3D1', label: 'Ruby'},
+        { value: data.NodeBPCount, color: '#46BFBD', highlight: '#5AD3D1', label: 'NodeJs'},
+        { value: data.GOBPCount, color: '#46BFBD', highlight: '#5AD3D1', label: 'GOLang'},
+        { value: data.PythonBPCount, color: '#46BFBD', highlight: '#5AD3D1', label: 'Python'},
+        { value: data.PHPBPCount, color: '#FDB45C', highlight: '#FFC870', label: 'PHP'},
+        { value: data.OtherBPCount, color: '#FDB45C', highlight: '#FFC870', label: 'Other'}
+      ];
+    }
 
     apps.getAppsInfo = function() {
       var responsePromise = $http.get(appsInfoURI);
@@ -30,6 +66,7 @@ angular.module('AppsApp', [])
         $scope.startedStateCount = data.StartedStateCount;
         $scope.diegoAppsCount = data.DiegoAppsCount;
         $scope.userInfoStatus = "";
+        apps.setDonutUX(data);
       });
 
       responsePromise.error(function(data, status, headers, config) {
